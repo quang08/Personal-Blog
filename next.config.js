cf436520ea62d50e10b3522e187799514050e182
Quang Nguyen The <quangnguyenthe@Quangs-MacBook-Air.local>
2022-12-24 10:46:23 +0700

    fixed

--- next.config.js
+++ next.config.js
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  images: {
-    domains: ["media.graphassets.com"],
-  },
-  ignoreDuringBuilds: true,
 };
 
 module.exports = nextConfig
